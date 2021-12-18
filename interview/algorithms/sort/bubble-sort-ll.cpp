e270435bfe49bef49f8fd507e25205568fb456fe
Your Name <you@example.com>
2021-12-19 00:00:12 +0530

    nc

--- interview/algorithms/sort/bubble-sort-ll.cpp
+++ interview/algorithms/sort/bubble-sort-ll.cpp
@@ -10,10 +10,37 @@ typedef struct node {
     struct node *next;
 } node;
 
-void bubble_sort_non_dec(int arr[], int size){    
+void bubble_sort_non_dec(node **head, int count){
+	int i, j;
+	node *p1, *p2;
+	for(i=0;i<count-1; i++){
+		swapped = false;
+		p1 = *head;
+		p2 = p1->next;
+		for(j=0;j<count-1-i;j++){			
+			if(p1->data > p2->data){
+				swap(p1, p2);
+				swapped = true;
+			}
+			new_head = 			
+		}
+		if(!swapped) {
+			break;
+		}
+	}
 }
 
 int main(int argc, char *argv[]){	
-	node *head = (node *)malloc(sizeof(node));	
+	node *head = (node *)malloc(sizeof(node));
+	head->data = 3;
+
+	head->next = (node *)malloc(sizeof(node));
+	head->next->data = 2;
+
+	head->next->next = (node *)malloc(sizeof(node));	
+	head->next->next->data = 1;
+
+	bubble_sort_non_dec(&head, 3);
+
 	return 0;
 }

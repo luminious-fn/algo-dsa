a8338a17a74feb0c372b1783c759b9b4d195468d
Your Name <you@example.com>
2021-12-18 23:50:44 +0530

    nc

---
+++ interview/algorithms/sort/bubble-sort-ll.cpp
@@ -0,0 +1,19 @@
+/*
+export PROG="bubble-sort-ll";clear; g++ $PROG.cpp -o $PROG.exe && ./$PROG.exe 
+*/
+
+#include<iostream>
+using namespace std;
+
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+void bubble_sort_non_dec(int arr[], int size){    
+}
+
+int main(int argc, char *argv[]){	
+	node *head = (node *)malloc(sizeof(node));	
+	return 0;
+}
